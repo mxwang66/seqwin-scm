c9083182c6c9a07aec0fabd4907d64f0e26602d9
mxwang66 <54554363+mxwang66@users.noreply.github.com>
2026-04-23 15:33:11 -0500

    Add docstrings

--- src/scm/__init__.py
+++ src/scm/__init__.py
@@ -1,28 +1,69 @@
+"""
+SCM
+===
+
+Fit set covering machine (SCM) models on Seqwin data structures (`kmers` and `nodes`).  
+
+Dependencies:
+-------------
+- numpy
+
+Classes:
+----------
+- SCMModel
+
+Functions:
+----------
+- fit
+"""
+
+__author__ = 'Michael X. Wang'
+__license__ = 'GPL 3.0'
+
 from dataclasses import dataclass
+
 import numpy as np
 from numpy.typing import NDArray
 
-# Import the compiled extension (built by pybind11)
 from ._core import fit_native
 
 @dataclass(frozen=True, slots=True)
 class SCMModel:
+    """The SCM model class. 
+
+    Attributes:
+        disjunction (str): True for a disjunction model. False for a conjunction model. 
+        nodes (NDArray[np.int64]): Indices of the selected nodes. 
+        polarities (NDArray[np.uint8]): SCM rule type of each selected node. 0 for presence, 1 for absence. 
+        pred (NDArray[np.uint8]): Predictions of the Seqwin assemblies. 0 for non-targets, 1 for targets. 
+    """
     disjunction: bool
     nodes: NDArray[np.int64]
     polarities: NDArray[np.uint8]
     pred: NDArray[np.uint8]
 
 def fit(
-    node_start: NDArray[np.uint64],
-    node_stop: NDArray[np.uint64],
-    kmer_assembly_idx: NDArray[np.uint16],
-    is_target: NDArray[np.uint8],
-    max_rules: int,
-    p: float = 1.0,
+    node_start: NDArray[np.uint64], 
+    node_stop: NDArray[np.uint64], 
+    kmer_assembly_idx: NDArray[np.uint16], 
+    is_target: NDArray[np.uint8], 
+    max_rules: int, 
+    p: float = 1.0, 
     disjunction: bool = True
 ) -> SCMModel:
-    """
-    Fit the SCM model. Inputs must be 1D NumPy arrays (C-contiguous) of the correct dtype.
+    """Fit the SCM model. Rules are labeled with node indices
+
+    Args:
+        node_start (NDArray[np.uint64]): C-contiguous copy of `nodes['start']`. 
+        node_stop (NDArray[np.uint64]): C-contiguous copy of `nodes['stop']`. 
+        kmer_assembly_idx (NDArray[np.uint16]): C-contiguous copy of `kmers['assembly_idx']`. 
+        is_target (NDArray[np.uint8]): `np.uint8` array converted from `assemblies['is_target']`. 
+        max_rules (int): Maximum number of rules for the SCM model. 
+        p (float, optional): SCM hyperparameter (penalty for the utility function). Should be no less than 1. [1.0]
+        disjunction (bool, optional): True to fit a disjunction model, False to fit a conjunction model. [True]
+
+    Returns:
+        SCMModel: The SCM model. Only works for the provided Seqwin data. 
     """
     dis, nodes, pol, pred = fit_native(
         node_start, node_stop, kmer_assembly_idx, is_target,
